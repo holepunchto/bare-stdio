6fc96160c00b511e6b2aa2bd4543d0ebb8bdbdf8
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-02-07 02:43:45 +0800

    Use fs.createReadStream and fs.createWriteStream

--- index.js
+++ index.js
@@ -3,37 +3,6 @@ const { Writable, Readable } = require('bare-stream')
 const fs = require('bare-fs')
 
 module.exports = new (class Stdio {
-  static WriteStream = class FdWriteStream extends Writable {
-    constructor(fd) {
-      super({ map: (data) => (typeof data === 'string' ? Buffer.from(data) : data) })
-      this.fd = fd
-    }
-
-    _writev(batch, cb) {
-      fs.writev(
-        this.fd,
-        batch.map(({ chunk }) => chunk),
-        cb
-      )
-    }
-  }
-
-  static ReadStream = class FdReadStream extends Readable {
-    constructor(fd) {
-      super()
-      this.fd = fd
-    }
-
-    _read(size) {
-      const buffer = Buffer.alloc(size)
-      fs.read(this.fd, buffer, 0, size, null, (err, bytesRead) => {
-        if (err) return this.destroy(err)
-        if (bytesRead === 0) return this.push(null)
-        this.push(buffer.slice(0, bytesRead))
-      })
-    }
-  }
-
   drained = Writable.drained
   constructor() {
     this._in = null
@@ -48,7 +17,7 @@ module.exports = new (class Stdio {
 
   get in() {
     if (this._in === null) {
-      this._in = tty.isTTY(0) ? new tty.ReadStream(0) : new this.constructor.ReadStream(0)
+      this._in = tty.isTTY(0) ? new tty.ReadStream(0) : fs.createReadStream(null, { fd: 0 })
       this._in.once('close', () => {
         this._in = null
       })
@@ -58,14 +27,14 @@ module.exports = new (class Stdio {
 
   get out() {
     if (this._out === null) {
-      this._out = tty.isTTY(1) ? new tty.WriteStream(1) : new this.constructor.WriteStream(1)
+      this._out = tty.isTTY(1) ? new tty.WriteStream(1) : fs.createWriteStream(null, { fd: 1 })
     }
     return this._out
   }
 
   get err() {
     if (this._err === null) {
-      this._err = tty.isTTY(2) ? new tty.WriteStream(2) : new this.constructor.WriteStream(2)
+      this._err = tty.isTTY(2) ? new tty.WriteStream(2) : fs.createWriteStream(null, { fd: 2 })
     }
     return this._err
   }
